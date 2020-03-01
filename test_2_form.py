1917c50c50cf7ecfd46b3b25f7803a4f89c64627
waszczukp <waszczukp@users.noreply.github.com>
2020-03-01 12:09:05 +0100

    test3

--- test_2_form.py
+++ test_2_form.py
@@ -5,9 +5,8 @@ import os
 import unittest
 from appium import webdriver
 from time import sleep
-#bierze path z miejsca w ktorym jest projekt
-from selenium.webdriver import ActionChains
 
+#bierze path z miejsca w ktorym jest projekt
 PATH = lambda p: os.path.abspath(
     os.path.join(os.path.dirname(__file__),p)
 )
@@ -20,9 +19,11 @@ class TestowanieAplikacji(unittest.TestCase):
         desired_caps['platformName'] = 'Android'
         desired_caps['platformVersion'] = '7.0'
         desired_caps['deviceName'] = 'Gigaset GS170'
-        desired_caps['app'] = PATH('ContactManager.apk')
-        desired_caps['appPackage'] = 'com.example.android.contactmanager'
-        desired_caps['appActivity'] = 'com.example.android.contactmanager.ContactManager'
+        desired_caps['app'] = PATH('ApiDemos-debug.apk')
+        desired_caps['appPackage'] = 'io.appium.android.apis'
+        desired_caps['appActivity'] = 'io.appium.android.apis.ApiDemos'
+        desired_caps['unicodeKeyboard'] = True
+        desired_caps['resetKeyboard'] = True
 
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
@@ -33,12 +34,15 @@ class TestowanieAplikacji(unittest.TestCase):
     #czesc z funkcjami testujacymi
 
     def test_is_app2_installed(self):
-        self.assertTrue(self.driver.is_app_installed('com.example.android.contactmanager')) #app package
+        self.assertTrue(self.driver.is_app_installed('io.appium.android.apis')) #app package
 
     def test_new_contact(self):
         add = self.driver.find_element_by_accessibility_id('Add Contact')
         add.click() #dla tych ktore maja content-desc
+
         textElements = self.driver.find_elements_by_class_name('android.widget.EditText')
+        elementsNumber = textElements.__len__()
+        self.assertEqual(elementsNumber, 3)
         textElements[0].send_keys('test contact')
         textElements[1].send_keys('123321456')
         textElements[2].send_keys('test@test.pl')
@@ -48,8 +52,57 @@ class TestowanieAplikacji(unittest.TestCase):
         # phone.send_keys('123321456')
         # email = self.driver.find_element_by_id('com.example.android.contactmanager:id/contactEmailEditText')
         # email.send_keys('test@test.pl')
-        submit = self.driver.find_element_by_accessibility_id('com.example.android.contactmanager:id/contactSaveButton')
-        submit.click()
+        # submit = self.driver
+        # .find_element_by_accessibility_id('com.example.android.contactmanager:id/contactSaveButton')
+        # submit.click()
+        self.assertEqual(textElements[0].text, 'test contact')
+        self.assertEqual(textElements[1].text, '123321456')
+        self.assertEqual(textElements[2].text, 'test@test.pl')
+
+        print("Liczba elementow z tekstem:")
+        print(textElements.__len__().__str__())
+
+        print(textElements[0].text)
+        print(textElements[1].text)
+        print(textElements[2].text)
+
+    def test_new_contact_with_work(self):
+        add = self.driver.find_element_by_accessibility_id('Add Contact')
+        add.click() #dla tych ktore maja content-desc
+        chooseElements = self.driver.find_elements_by_class_name('//android.widget.Spinner[@text="Home"]')
+        chooseElementsNumber = chooseElements.__len__()
+        self.assertEqual(chooseElementsNumber, 2)
+
+        textElements = self.driver.find_elements_by_class_name('android.widget.EditText')
+        textElementsNumber = textElements.__len__()
+        self.assertEqual(textElementsNumber, 3)
+
+        chooseElements[1].click()
+        textElements[0].send_keys('test contact')
+        chooseElements[1].click()
+        textElements[1].send_keys('123321456')
+        chooseElements[2].click()
+        textElements[2].send_keys('test@test.pl')
+        # name = self.driver.find_element_by_id('com.example.android.contactmanager:id/contactNameEditText')
+        # name.send_keys('test contact')
+        # phone = self.driver.find_element_by_id('com.example.android.contactmanager:id/contactPhoneEditText')
+        # phone.send_keys('123321456')
+        # email = self.driver.find_element_by_id('com.example.android.contactmanager:id/contactEmailEditText')
+        # email.send_keys('test@test.pl')
+        # submit = self.driver
+        # .find_element_by_accessibility_id('com.example.android.contactmanager:id/contactSaveButton')
+        # submit.click()
+        self.assertEqual(textElements[0].text, 'test contact')
+        self.assertEqual(textElements[1].text, '123321456')
+        self.assertEqual(textElements[2].text, 'test@test.pl')
+
+        print("Liczba elementow z tekstem:")
+        print(textElements.__len__().__str__())
+
+        print(textElements[0].text)
+        print(textElements[1].text)
+        print(textElements[2].text)
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestowanieAplikacji) #musi sie zgadzac z nazwa klasy
---
+++ test_3_WIFI.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8" -*
+
+
+import os
+import unittest
+from appium import webdriver
+from time import sleep
+
+#bierze path z miejsca w ktorym jest projekt
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__),p)
+)
+
+
+class TestowanieAplikacji(unittest.TestCase):
+    #czesc z funkcjami konfigurujacymi
+    def setUp(self):
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '7.0'
+        desired_caps['deviceName'] = 'Gigaset GS170'
+        desired_caps['app'] = PATH('ApiDemos-debug.apk')
+        desired_caps['appPackage'] = 'io.appium.android.apis'
+        desired_caps['appActivity'] = 'io.appium.android.apis.ApiDemos'
+        desired_caps['unicodeKeyboard'] = True
+        desired_caps['resetKeyboard'] = True
+
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+    #czesc z funkcjami testujacymi
+
+    # def test_is_installed(self):
+    #     self.assertTrue(self.driver.is_app_installed('io.appium.android.apis')) #app package
+
+    def test_WIFI_connection(self):
+        self.driver.find_element_by_xpath('//android.widget.TextView[@text="Preference"]').click()
+        self.driver.find_element_by_xpath('//android.widget.TextView[@text="3. Preference dependencies"]').click()
+        checkBoxes = self.driver.find_elements_by_class_name('android.widget.CheckBox')
+        checkBoxesNumber = checkBoxes.__len__()
+        print(checkBoxesNumber)
+        self.assertEqual(checkBoxesNumber, 1)
+        checkBoxes[0].click()
+        self.driver.find_element_by_xpath('//android.widget.TextView[@text="WiFi settings"]').click()
+        self.driver.find_element_by_id('android:id/edit').send_keys('123321789')
+        self.driver.find_element_by_xpath('//android.widget.Button[@text="OK"]').click()
+
+        # self.driver.find_elements_by_xpath('//android.widget.TextView[@text="]').click()
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestowanieAplikacji) #musi sie zgadzac z nazwa klasy
+    unittest.TextTestRunner(verbosity=2).run(suite)
